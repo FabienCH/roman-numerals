e88186381c4addf63b11136e3b93702cec9c3e30
FabienCH <fabien.charvaz@gmail.com>
2020-12-28 16:50:26 +0100

    refactor: add convertFromOneToFour function

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-export function convertToRoman(number: number): string {
-  if (number === 5) {
-    return 'V';
-  }
-
-  if (number === 6) {
-    return 'VI';
-  }
-
+function convertFromOneToFour(number: number): string {
   return new Array(number).fill(null).reduce((romanNumber: string, _) => {
     romanNumber += 'I';
     return romanNumber;
   }, '');
+}
+
+export function convertToRoman(number: number): string {
+  if (number / 5 >= 1) {
+    const remainder = number % 5;
+    return `V${convertFromOneToFour(remainder)}`;
+  }
+
+  return convertFromOneToFour(number);
 }
