9a8c072178e77b63e32cc90e17a4de2842b55ac5
FabienCH <fabien.charvaz@gmail.com>
2020-12-28 16:55:36 +0100

    refactor: add RomanNumeralsConverter class

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,20 +1,25 @@
-import { convertToRoman } from './index';
+import { RomanNumeralsConverter } from './index';
 
 describe('convertToRoman', () => {
+  let romanNumeralsConverter: RomanNumeralsConverter;
+
+  beforeEach(() => {
+    romanNumeralsConverter = new RomanNumeralsConverter();
+  });
   it('should convert 1 to "I"', () => {
-    expect(convertToRoman(1)).toEqual('I');
+    expect(romanNumeralsConverter.convertToRoman(1)).toEqual('I');
   });
   it('should convert 2 to "II"', () => {
-    expect(convertToRoman(2)).toEqual('II');
+    expect(romanNumeralsConverter.convertToRoman(2)).toEqual('II');
   });
   it('should convert 3 to "III"', () => {
-    expect(convertToRoman(3)).toEqual('III');
+    expect(romanNumeralsConverter.convertToRoman(3)).toEqual('III');
   });
   it('should convert 5 to "V"', () => {
-    expect(convertToRoman(5)).toEqual('V');
+    expect(romanNumeralsConverter.convertToRoman(5)).toEqual('V');
   });
 
   it('should convert 6 to "VI"', () => {
-    expect(convertToRoman(6)).toEqual('VI');
+    expect(romanNumeralsConverter.convertToRoman(6)).toEqual('VI');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-function convertFromOneToFour(number: number): string {
-  return new Array(number).fill(null).reduce((romanNumber: string, _) => {
-    romanNumber += 'I';
-    return romanNumber;
-  }, '');
-}
+export class RomanNumeralsConverter {
+  private readonly one = 'I';
+  private readonly five = 'V';
 
-export function convertToRoman(number: number): string {
-  if (number / 5 >= 1) {
-    const remainder = number % 5;
-    return `V${convertFromOneToFour(remainder)}`;
+  public convertToRoman(number: number): string {
+    if (number / 5 >= 1) {
+      const remainder = number % 5;
+      return `${this.five}${this.convertFromOneToFour(remainder)}`;
+    }
+
+    return this.convertFromOneToFour(number);
   }
 
-  return convertFromOneToFour(number);
+  private convertFromOneToFour(number: number): string {
+    return new Array(number).fill(null).reduce((romanNumber: string, _) => {
+      romanNumber += this.one;
+      return romanNumber;
+    }, '');
+  }
 }
