f6993c685450d2a4e4e90285b5154808ec2cb767
FabienCH <fabien.charvaz@gmail.com>
2020-12-29 11:58:53 +0100

    refactor: add prependedNumber method

--- src/index.ts
+++ src/index.ts
@@ -9,35 +9,28 @@ export class RomanNumeralsConverter {
   public convertToRoman(number: number): string {
     this.remainder = number;
 
-    if (this.remainder === 39) {
-      this.romanValue = 'XXXIX';
-      this.remainder -= 39;
-    }
-
     while (this.remainder >= 50) {
       this.romanValue += this.fifty;
       this.remainder -= 50;
+      this.prependNumber(9, this.ten);
+      this.prependNumber(4, this.five);
     }
 
-    if (this.remainder === 9) {
-      this.romanValue += `${this.one}${this.ten}`;
-      this.remainder -= 9;
-    }
+    this.prependNumber(9, this.ten);
 
     while (this.remainder >= 10) {
       this.romanValue += this.ten;
       this.remainder -= 10;
+      this.prependNumber(9, this.ten);
+      this.prependNumber(4, this.five);
     }
 
-    if (this.remainder === 4) {
-      const romanValue = this.romanValue ? this.romanValue : this.five;
-      this.romanValue += `${this.one}${romanValue}`;
-      this.remainder -= 4;
-    }
+    this.prependNumber(4, this.five);
 
     if (this.remainder >= 5) {
       this.romanValue += this.five;
       this.remainder = this.remainder % 5;
+      this.prependNumber(4, this.five);
     }
 
     while (this.remainder >= 1) {
@@ -46,5 +39,12 @@ export class RomanNumeralsConverter {
     }
 
     return this.romanValue;
+  }
+
+  private prependNumber(number: number, suffix: string): void {
+    if (this.remainder === number) {
+      this.romanValue += `${this.one}${suffix}`;
+      this.remainder -= number;
+    }
   }
 }
