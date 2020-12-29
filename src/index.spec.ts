0843bbc624c467df2e9e4a3f9a2a8c903543101f
FabienCH <fabien.charvaz@gmail.com>
2020-12-29 10:24:54 +0100

    refactor: iterates over test cases

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,38 +6,22 @@ describe('convertToRoman', () => {
   beforeEach(() => {
     romanNumeralsConverter = new RomanNumeralsConverter();
   });
-  it('should convert 1 to "I"', () => {
-    expect(romanNumeralsConverter.convertToRoman(1)).toEqual('I');
-  });
-  it('should convert 2 to "II"', () => {
-    expect(romanNumeralsConverter.convertToRoman(2)).toEqual('II');
-  });
-  it('should convert 3 to "III"', () => {
-    expect(romanNumeralsConverter.convertToRoman(3)).toEqual('III');
-  });
-  it('should convert 4 to "IV"', () => {
-    expect(romanNumeralsConverter.convertToRoman(4)).toEqual('IV');
-  });
-  it('should convert 5 to "V"', () => {
-    expect(romanNumeralsConverter.convertToRoman(5)).toEqual('V');
-  });
 
-  it('should convert 6 to "VI"', () => {
-    expect(romanNumeralsConverter.convertToRoman(6)).toEqual('VI');
-  });
-  it('should convert 7 to "VIII"', () => {
-    expect(romanNumeralsConverter.convertToRoman(7)).toEqual('VII');
-  });
-  it('should convert 8 to "VIII"', () => {
-    expect(romanNumeralsConverter.convertToRoman(8)).toEqual('VIII');
-  });
-  it('should convert 9 to "IX"', () => {
-    expect(romanNumeralsConverter.convertToRoman(9)).toEqual('IX');
-  });
-  it('should convert 10 to "X"', () => {
-    expect(romanNumeralsConverter.convertToRoman(10)).toEqual('X');
-  });
-  it('should convert 11 to "XI"', () => {
-    expect(romanNumeralsConverter.convertToRoman(11)).toEqual('XI');
+  [
+    { number: 1, expectedRomanNumber: 'I' },
+    { number: 2, expectedRomanNumber: 'II' },
+    { number: 3, expectedRomanNumber: 'III' },
+    { number: 4, expectedRomanNumber: 'IV' },
+    { number: 5, expectedRomanNumber: 'V' },
+    { number: 6, expectedRomanNumber: 'VI' },
+    { number: 7, expectedRomanNumber: 'VII' },
+    { number: 8, expectedRomanNumber: 'VIII' },
+    { number: 9, expectedRomanNumber: 'IX' },
+    { number: 10, expectedRomanNumber: 'X' },
+    { number: 11, expectedRomanNumber: 'XI' },
+  ].forEach(({ number, expectedRomanNumber }) => {
+    it(`should convert ${number} to "${expectedRomanNumber}"`, () => {
+      expect(romanNumeralsConverter.convertToRoman(number)).toEqual(expectedRomanNumber);
+    });
   });
 });
