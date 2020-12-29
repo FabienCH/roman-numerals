d2d00f109a487819e6b70ce68f76f0cdf6e90efb
FabienCH <fabien.charvaz@gmail.com>
2020-12-29 11:01:27 +0100

    refactor: add method to repeat symbol one to three times

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ export class RomanNumeralsConverter {
   private readonly ten = 'X';
 
   public convertToRoman(number: number): string {
+    const divByTen = number / 10;
     const divByFiveRemainder = number % 5;
     const isPrepended = divByFiveRemainder === 4;
     const isMoreThanFour = number > 4;
     const isSuffixed = isMoreThanFour && divByFiveRemainder < 4;
 
     if (number === 20) {
-      return 'XX';
+      return this.repeatSymbolOneToThreeTimes(divByTen, this.ten);
     }
 
     if (isPrepended) {
@@ -18,11 +19,11 @@ export class RomanNumeralsConverter {
     }
 
     if (isSuffixed) {
-      const suffix = number - 10 > 4 ? this.five : this.convertFromOneToFour(divByFiveRemainder);
+      const suffix = number - 10 > 4 ? this.five : this.repeatSymbolOneToThreeTimes(divByFiveRemainder, this.one);
       return `${this.getPrependedNumber(number < 10)}${suffix}`;
     }
 
-    return this.convertFromOneToFour(number);
+    return this.repeatSymbolOneToThreeTimes(number, this.one);
   }
 
   private getSuffixedNumber(isMoreThanFour: boolean) {
@@ -33,9 +34,9 @@ export class RomanNumeralsConverter {
     return isLessThanTen ? this.five : this.ten;
   }
 
-  private convertFromOneToFour(number: number): string {
-    return new Array(number).fill(null).reduce((romanNumber: string, _) => {
-      romanNumber += this.one;
+  private repeatSymbolOneToThreeTimes(numberOfTimes: number, symbol: string): string {
+    return new Array(numberOfTimes).fill(null).reduce((romanNumber: string, _) => {
+      romanNumber += symbol;
       return romanNumber;
     }, '');
   }
