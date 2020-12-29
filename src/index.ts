e95e732234d162b8450b3500b4d1824374047fbe
FabienCH <fabien.charvaz@gmail.com>
2020-12-29 11:26:02 +0100

    refactor: use remainder to convert all values

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,26 @@ export class RomanNumeralsConverter {
   public convertToRoman(number: number): string {
     this.remainder = number;
 
-    if (number >= 10) {
-      const divByTen = number / 10;
+    if (this.remainder === 9) {
+      this.romanValue += `${this.one}${this.ten}`;
+      this.remainder -= 9;
+    }
+
+    if (this.remainder >= 10) {
+      const divByTen = this.remainder / 10;
       this.romanValue += this.repeatSymbolOneToThreeTimes(Math.trunc(divByTen), this.ten);
-      this.remainder = number % 10;
+      this.remainder = this.remainder % 10;
     }
 
-    const divByFiveRemainder = this.remainder % 5;
-    const isPrepended = divByFiveRemainder === 4;
-    const isMoreThanFour = this.remainder > 4;
-    const isSuffixed = isMoreThanFour && divByFiveRemainder < 4;
-
-    if (isPrepended) {
-      this.romanValue += `${this.one}${this.getSuffixedNumber(isMoreThanFour)}`;
+    if (this.remainder === 4) {
+      const romanValue = this.romanValue ? this.romanValue : this.five;
+      this.romanValue += `${this.one}${romanValue}`;
     }
 
-    if (isSuffixed) {
-      const suffix = this.remainder - 10 > 4 ? this.five : this.repeatSymbolOneToThreeTimes(divByFiveRemainder, this.one);
-      this.romanValue += `${this.getPrependedNumber(this.remainder < 10)}${suffix}`;
+    if (this.remainder >= 5) {
+      const divByTen = this.remainder / 5;
+      this.romanValue += this.repeatSymbolOneToThreeTimes(Math.trunc(divByTen), this.five);
+      this.remainder = this.remainder % 5;
     }
 
     if (this.remainder <= 3) {
@@ -33,14 +35,6 @@ export class RomanNumeralsConverter {
     }
 
     return this.romanValue;
-  }
-
-  private getSuffixedNumber(isMoreThanFour: boolean) {
-    return isMoreThanFour ? this.ten : this.five;
-  }
-
-  private getPrependedNumber(isLessThanTen: boolean) {
-    return isLessThanTen ? this.five : this.ten;
   }
 
   private repeatSymbolOneToThreeTimes(numberOfTimes: number, symbol: string): string {
