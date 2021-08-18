0ae80b7b1c3ea8a3194a72c2312df4020680e39c
FabienCH <fabien.charvaz@gmail.com>
2021-08-18 22:03:49 +0200

    refactor: extract getSymbolWithLargerSuffix method

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,22 @@ function convertOneToEight(number: number): string {
   return `V${convertOneToThree(number - 5)}`;
 }
 
+function getRegularSymbol(number: number): string {
+  return number >= 5 ? convertOneToEight(number) : convertOneToThree(number);
+}
+
+function getSymbolWithLargerSuffix(number: number): string {
+  return number === 4 ? 'IV' : 'IX';
+}
+
 export function convertToRomanNumber(number: number): string {
-  if (number === 4) {
-    return 'IV';
-  }
-  if (number === 9) {
-    return 'IX';
-  }
-  if (number === 14) {
-    return 'XIV';
-  }
-  if (number === 19) {
-    return 'XIX';
-  }
   if (number >= 10) {
     number -= 10;
-    const suffix = number >= 5 ? convertOneToEight(number) : convertOneToThree(number);
+    const suffix = number % 5 === 4 ? getSymbolWithLargerSuffix(number) : getRegularSymbol(number);
     return `X${suffix}`;
+  }
+  if (number % 5 === 4) {
+    return getSymbolWithLargerSuffix(number);
   }
   if (number >= 5) {
     return convertOneToEight(number);
