0c196607d69f3225a92f081042a8664760095993
FabienCH <fabien.charvaz@gmail.com>
2021-08-18 21:11:15 +0200

    feat: convert number to roman for 4 to 9

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,6 +5,12 @@ describe('convertToRomanNumber', () => {
     { input: 1, expected: 'I' },
     { input: 2, expected: 'II' },
     { input: 3, expected: 'III' },
+    { input: 4, expected: 'IIII' },
+    { input: 5, expected: 'V' },
+    { input: 6, expected: 'VI' },
+    { input: 7, expected: 'VII' },
+    { input: 8, expected: 'VIII' },
+    { input: 9, expected: 'VIIII' },
   ];
 
   testParams.forEach((param) => {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 export function convertToRomanNumber(number: number): string {
+  if (number === 9) {
+    return 'VIIII';
+  }
+  if (number === 8) {
+    return 'VIII';
+  }
+  if (number === 7) {
+    return 'VII';
+  }
+  if (number === 6) {
+    return 'VI';
+  }
+  if (number === 5) {
+    return 'V';
+  }
   return Array.from({ length: number }, (_, i) => i).reduce((romanNumber) => (romanNumber += 'I'), '');
 }
